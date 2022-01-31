60c3b3b3274cbe564863f02710824c2d9c22d613
Dima Semenov <dsemenov@asist.pl>
2022-01-31 12:41:13 +0100

    added state to faq-page

--- src/components/tutorial-2-2/index.js
+++ src/components/tutorial-2-2/index.js
@@ -1,14 +1,18 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
+
+//styles
 import "./index.css";
 
+//data
+import { tabs } from "../../utils";
+
 const Faq = () => {
-  let refActiveId = useRef();
   const [active, setActive] = useState("");
 
   const handleClick = (e) => {
-    setActive(e.target.name);
+    setActive(e.target.id);
   };
-  console.log(active);
+
   return (
     <>
       <div id="app">
@@ -16,19 +20,17 @@ const Faq = () => {
           <h1 className="app-title">FAQ</h1>
           <div className="app-tabs">
             {tabs.length
-              ? tabs.map((tabInfo) => (
+              ? tabs.map((tab) => (
                   <div
-                    key={tabInfo.id}
-                    ref={refActiveId}
-                    id={tabInfo.id}
-                    className={`tab active`}
+                    key={tab.id}
+                    className={`tab ${+active === tab.id ? "active" : ""}`}
                   >
                     <input id="tab-one" type="checkbox" name="tabs" />
-                    <label htmlFor="tab-one" onClick={handleClick}>
-                      {tabInfo.title}
+                    <label htmlFor="tab-one" id={tab.id} onClick={handleClick}>
+                      {tab.title}
                     </label>
                     <div className="tab-content">
-                      <p>{tabInfo.description}</p>
+                      <p>{tab.description}</p>
                     </div>
                   </div>
                 ))
@@ -39,35 +41,5 @@ const Faq = () => {
     </>
   );
 };
-
-const tabs = [
-  {
-    id: 1,
-    title: "Сколько всего мест в доме?",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nemo corporis velit culpa veritatis asperiores deserunt, commodi ipsum at? Esse quibusdam dignissimos recusandae enim. Eaque expeditaeum provident totam!",
-  },
-
-  {
-    id: 2,
-    title: "Самая дорогая квартира?",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fuga deserunt aliquid voluptatum ad, molestiae dicta officiis animi ummollitia, reiciendis a cum ratione veritatis cupiditate voluptatem.Consectetur, exercitationem magnam.",
-  },
-
-  {
-    id: 3,
-    title: "Могу ли я отменить бронирование?",
-    description:
-      "Да, вы можете отменить забронированную квартиру в течение 3 дней.",
-  },
-
-  {
-    id: 4,
-    title: "Можно ли купить квартиру?",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi itaque ratione voluptatem dolores iusto aperiam adipisci incidunt perspiciatis ullam! Repellat accusamus rerum excepturi minus delectus? Consequatur libero nemo alias.",
-  },
-];
 
 export default Faq;
